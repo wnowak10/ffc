9a18a6fdb145ddeaac5749d6343a306dd47be786
Philip Kalikman <pkalikman@users.noreply.github.com>
2017-04-08 14:22:52 -0400

    Update ff.py

--- ff.py
+++ ff.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+#Inline edit!
 np.random.seed(1234)
 print('reading in csv files')
 # File read in
