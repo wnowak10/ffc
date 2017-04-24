a732c5777bd7b6554647d47a5cfd556d821a63db
wnowak <wnowak10@gmail.com>
2017-04-24 17:42:33 -0400

    no substantive changes, but tidied .ipynb file and updated .py file accordingly. now good to import df_train, non_na_y_train_materialHardship, and df_prediction from this .py file elsewhere.

--- materialHardship/data_prepare.py
+++ materialHardship/data_prepare.py
@@ -3,16 +3,17 @@
 
 # # Prepare data (just for materialHardship)
 
-# In[1]:
+# In[2]:
 
 import numpy as np
 import pandas as pd
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
+# import xgboost as xgb
+# from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-# In[4]:
+# In[3]:
 
 np.random.seed(1234)
 print('reading in csv files')
@@ -30,7 +31,7 @@ print("Files read")
 
 # Background is super WIDE. We have only 4242 IDs, but almost 13k features!
 
-# In[5]:
+# In[4]:
 
 background.shape
 
@@ -39,7 +40,7 @@ background.shape
 # 
 # Then we need to make predictions for every challengeID that is a) not train or b) is in train by has NA for materialHardship 
 
-# In[35]:
+# In[5]:
 
 all_y_train_materialHardship = train[['challengeID', 'materialHardship']]
 # non NA y_train ds and data
@@ -47,7 +48,7 @@ non_na_y_train_materialHardship = all_y_train_materialHardship.dropna()
 non_na_y_train_materialHardship.head()
 
 
-# In[34]:
+# In[6]:
 
 # y_train_ids that are na
 na_y_train_materialHardship = all_y_train_materialHardship.loc[~all_y_train_materialHardship['challengeID'].isin(non_na_y_train_materialHardship['challengeID'])]
@@ -56,38 +57,58 @@ na_y_train_materialHardship.head()
 
 # Our training set will have all ids that we have non-NA material hardship data for. 
 
-# In[19]:
+# In[7]:
 
 df_train = background.loc[background['challengeID'].isin(non_na_y_train_materialHardship['challengeID'])]
 df_train.head()
 
 
-# In[32]:
+# The prediction set contains all rows in 'background.csv' that are not in df_train.
 
-# prediction set
-# all ids in background, excluding train_ids
-background_non_train_ids = background.loc[~background['challengeID'].isin(train['challengeID'])]
+# In[9]:
 
-# add back in ids that had NA in material hardship train set
-background_mh_nas = background.loc[background['challengeID'].isin(na_y_train_materialHardship['challengeID'])]
-# background_mh_nas.head()
-
-# combine 
-df_prediction = background_non_train_ids.append(background_mh_nas)
+df_prediction = background.loc[~background['challengeID'].isin(df_train['challengeID'])]
 df_prediction.sort_values(by='challengeID').head(10)
 
 
 # SO, we have the data that we will train on:
 #     
-#     - 'df_train' with labels 'non_na_y_train_materialHardship'
-#     - We'll use this model to predict output (materialHardship) values for 'df_prediction'
+#     - Training features: 'df_train' 
+#     - Training labels: 'non_na_y_train_materialHardship'
+# 
+# We'll use this model to predict output (materialHardship) values for:
+# 
+#     - Prediction features: 'df_prediction'
 
-# In[1]:
+# ### df_train
+
+# In[11]:
 
 df_train.head()
+
+
+# ### non_na_y_train_materialHardship
+
+# In[12]:
+
 non_na_y_train_materialHardship.head()
+
+
+# ### df_prediction
+
+# In[13]:
+
 df_prediction.head()
 
+
+# We should have that the sum of rows in df_train and df_prediction sum to all of the rows in background.csv. Let's ensure that.
+
+# In[15]:
+
+np.sum(df_train.shape[0]+df_prediction.shape[0])==background.shape[0]
+
+
+# OK. We are off to the races and can reference these pandas dataframes in future data preparation and training notebooks!
 
 # In[ ]:
 
